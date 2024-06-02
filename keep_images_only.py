bce304e20ea4eba4c4d05b923a9544c4f42c18f2
Omkar <omgolatkar2001@gmail.com>
2024-06-02 13:22:23 +0530

    sanity check

--- keep_images_only.py
+++ keep_images_only.py
@@ -1,15 +1,22 @@
 import os
 import shutil
+from pathlib import Path
 
-dir_name = ".\\all_images\\Biomolecules Free Full-Text Actin Bundles Dynamics and Architecture_files"
-test = os.listdir(dir_name)
-
+dir_name = "./all_images"
+html_directory = "./papers"
+test = os.listdir(html_directory)
 
 for item in test:
-	if not item.endswith(".png"):
-		try:
-			os.remove(os.path.join(dir_name, item))
-		except OSError as error:
-			print(error)
-			shutil.rmtree(os.path.join(dir_name, item))
-			print("File path can not be removed")
+    if os.path.isdir(os.path.join(html_directory, item)):
+            Path(os.path.join(dir_name, item)).mkdir(parents=True, exist_ok=True)
+            for files in os.listdir(os.path.join(html_directory, item)):
+                    if files.endswith((".png", ".jpg", ".jpeg", ".gif")):
+                            print(files)
+                            shutil.copy(os.path.join(html_directory, item, files), os.path.join(dir_name, item))
+	# if not item.endswith(".png"):
+	# 	try:
+	# 		os.remove(os.path.join(dir_name, item))
+	# 	except OSError as error:
+	# 		print(error)
+	# 		shutil.rmtree(os.path.join(dir_name, item))
+	# 		print("File path can not be removed")
